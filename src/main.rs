8b681c3a9a25d162bce1dbe2ec552cd72c945cfc
Guilherme <sitegui@sitegui.com.br>
2020-12-08 19:23:57 +0100

    Run all days 10 times, then measure total time of more 10 executions
    This will reduce a little the total measurement noise. I should probably
    use `criterion` for serious measurement here :)

--- src/main.rs
+++ src/main.rs
@@ -54,17 +54,30 @@ days! {
 fn main() {
     match args().nth(1) {
         None => {
-            println!("Will execute all days");
-            let start = Instant::now();
-            for day in DAYS {
-                day.assert_solve();
+            println!("Will execute all days to time their total execution time");
+
+            // Warm caches
+            for _ in 0..10 {
+                solve_all();
             }
-            println!("{} days solved in {:?}", DAYS.len(), start.elapsed());
+
+            // Run
+            let start = Instant::now();
+            for _ in 0..10 {
+                solve_all();
+            }
+            println!("{} days solved in {:?}", DAYS.len(), start.elapsed() / 10);
         }
         Some(day) => {
             let day: usize = day.parse().unwrap();
             let (part_1, part_2) = DAYS[day - 1].solve();
             println!("Part 1 = {}, part 2 = {}", part_1, part_2);
         }
+    }
+}
+
+fn solve_all() {
+    for day in DAYS {
+        day.assert_solve();
     }
 }
