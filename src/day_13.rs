dbb7038a7aabd9b679b48c100b673abfd54764e9
Guilherme <sitegui@sitegui.com.br>
2020-12-13 14:53:37 +0100

    Comment day 13

--- src/day_13.rs
+++ src/day_13.rs
@@ -23,9 +23,25 @@ pub fn solve() -> (i64, i64) {
                 diff => bus - diff,
             };
 
-            // Calculate the nest matching departure, for part 2
-            let column = column as i64;
-            let target_delay = (-column).rem_euclid(bus);
+            // Calculate the nest matching departure, for part 2. `matching_departure` is the answer
+            // so far, considering the buses before this one. If that answer is increased of
+            // `matching_steps`, it is still a valid answer. However, it is no longer the *least*
+            // possible value
+            // For this new bus, we need to guarantee one extra condition:
+            // `new_matching_departure + column = K * bus`, for some integer K
+            // Taking module `bus` from both sides:
+            // `new_matching_departure + column = 0 (mod bus)`
+            // Rewriting to have a simpler equation:
+            // `new_matching_departure % bus = reminder(-column, bus)`, where `reminder()` gives a
+            // positive reminder, even for negative arguments.
+            // Since we want to respect this new constraint, while respecting the previous ones, we
+            // must have:
+            // `new_matching_departure = matching_departure + K * matching_steps`, so we simply
+            // iterate over values for `K` to find the answer :)
+            // Then we know that `new_matching_steps` is the greatest common multiplier of all buses
+            // so far. By inspection, they seem to be prime numbers, so that greatly simplify to
+            // their product.
+            let target_delay = (-(column as i64)).rem_euclid(bus);
             while matching_departure % bus != target_delay as i64 {
                 matching_departure += matching_steps;
             }
