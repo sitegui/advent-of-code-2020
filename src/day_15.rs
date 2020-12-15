87eca414d332641e473b9a606f049edef5d1a328
Guilherme <sitegui@sitegui.com.br>
2020-12-15 22:08:02 +0100

    Use `Vec` instead of `BTreeMap`, now 15 days in 551.452 ± 1.371 ms

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,37 +1,57 @@
 use crate::data::Data;
 use crate::iter_utils::IterUtils;
 use itertools::Itertools;
-use std::collections::BTreeMap;
+use std::mem;
 use std::ops::Range;
 
 const PART_1_TURNS: i32 = 2_020;
 const PART_2_TURNS: i32 = 30_000_000;
+const MAX_VALUE: i32 = 30_000_000;
+
+struct Memory {
+    /// `last_turns[value]` indicates the number of the last turn in which this value was saw, or
+    /// zero if never.
+    last_turns: Vec<i32>,
+}
 
 pub fn solve() -> (i64, i64) {
     let data = Data::read(15);
     let starting = data.split(b',', true).parsed::<i32>().collect_vec();
 
-    let mut memory = BTreeMap::new();
+    let mut memory = Memory::new();
 
     for (turn_from_zero, &value) in starting[..starting.len() - 1].iter().enumerate() {
         let turn = turn_from_zero as i32 + 1;
-        assert!(memory.insert(value, turn).is_none());
+        assert_eq!(memory.insert(value, turn), 0);
     }
 
     let next = starting[starting.len() - 1];
-    let start_turn = memory.len() as i32 + 1;
+    let start_turn = starting.len() as i32;
     let part_1 = play(&mut memory, start_turn..PART_1_TURNS, next);
     let part_2 = play(&mut memory, PART_1_TURNS..PART_2_TURNS, part_1);
 
     (part_1 as i64, part_2 as i64)
 }
 
-fn play(memory: &mut BTreeMap<i32, i32>, turns: Range<i32>, mut next: i32) -> i32 {
+fn play(memory: &mut Memory, turns: Range<i32>, mut next: i32) -> i32 {
     for turn in turns {
-        match memory.insert(next, turn) {
-            None => next = 0,
-            Some(prev_round) => next = turn - prev_round,
-        }
+        next = memory.insert(next, turn);
     }
     next
+}
+
+impl Memory {
+    fn new() -> Self {
+        Memory {
+            last_turns: vec![0; MAX_VALUE as usize],
+        }
+    }
+
+    /// Insert in the memory and return its age (or zero if new)
+    fn insert(&mut self, value: i32, turn: i32) -> i32 {
+        match mem::replace(&mut self.last_turns[value as usize], turn) {
+            0 => 0,
+            last_turn => turn - last_turn,
+        }
+    }
 }
